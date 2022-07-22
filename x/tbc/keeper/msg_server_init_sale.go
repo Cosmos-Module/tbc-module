ea0e85763ac425881c5f4f894af2553353ce2940
jaebok <jaebok.lee@modori.io>
2022-07-23 01:39:59 +0900

    impl init-sale msg

--- x/tbc/keeper/msg_server_init_sale.go
+++ x/tbc/keeper/msg_server_init_sale.go
@@ -3,15 +3,73 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"tbc/x/tbc/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) InitSale(goCtx context.Context, msg *types.MsgInitSale) (*types.MsgInitSaleResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	{
+		initialSupply, err := sdk.ParseCoinNormalized(msg.InitialSupply)
+		if err != nil {
+			panic(err)
+		}
+
+		maxSupply, err := sdk.ParseCoinNormalized(msg.MaxSupply)
+		if err != nil {
+			panic(err)
+		}
+
+		// Check each amounts
+		if initialSupply.IsGTE(maxSupply) {
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Initial supply exceeds max supply")
+		}
+	}
+
+	{
+		presalePrice, err := sdk.ParseCoinNormalized(msg.PresalePrice)
+		if err != nil {
+			panic(err)
+		}
+
+		maxPrice, err := sdk.ParseCoinNormalized(msg.MaxPrice)
+		if err != nil {
+			panic(err)
+		}
+
+		// Check each amounts
+		if presalePrice.IsGTE(maxPrice) {
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Initial price exceeds Max price")
+		}
+
+	}
+
+	// Parse coin from the message
+	creatorCoin, _ := sdk.ParseCoinNormalized(msg.MaxSupply)
+
+	// Throw error if coin already exists
+	_, isFound := k.GetCreatorCoin(ctx, creatorCoin.Denom)
+	if isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "creator coin already in used")
+	}
+
+	// Mint creator coin
+	creatorCoins := sdk.NewCoins(creatorCoin)
+	k.bankKeeper.MintCoins(ctx, types.ModuleName, creatorCoins)
+
+	// Set CreatorCoin
+	newTokenSale := types.CreatorCoin{
+		Index:         creatorCoin.Denom,
+		Creator:       msg.Creator,
+		InitialSupply: msg.InitialSupply,
+		MaxSupply:     msg.MaxSupply,
+		PresalePrice:  msg.PresalePrice,
+		MaxPrice:      msg.MaxPrice,
+	}
+	k.SetCreatorCoin(ctx, newTokenSale)
 
 	return &types.MsgInitSaleResponse{}, nil
 }
--- x/tbc/types/expected_keepers.go
+++ x/tbc/types/expected_keepers.go
@@ -15,4 +15,12 @@ type AccountKeeper interface {
 type BankKeeper interface {
 	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
 	// Methods imported from bank should be defined here
+	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+    SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
+	GetSupply(ctx sdk.Context, denom string) sdk.Coin
+    HasSupply(ctx sdk.Context, denom string) bool
+	MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+	GetAllBalances(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
+	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
+	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 }
